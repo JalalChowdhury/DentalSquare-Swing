fda9846905da1dde67d5ce426d49929e37b4c131
Md. Jalal Uddin Chowdhury <JalalChowdhuryCSE1999@gmail.com>
2021-10-27 02:05:21 +0600

    added bookings

--- src/Bookings.java
+++ src/Bookings.java
@@ -1,19 +1,31 @@
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Font;
 
 import javax.swing.*;    
-public class Bookings {    
-    JFrame f;    
+public class Bookings extends JFrame {  
+	private Container cont;
+//    JFrame f;    
     Bookings(){    
-    f=new JFrame();    
+//    f=new JFrame();    
     
-    f.setSize(1200,900);   
-    f.setTitle("Dental Square");
-//    JLabel headerText = new JLabel("Welcome To Dental Square And Create Your Account");
-//    headerText.setBounds(100,10,800,50);
-//	headerText.setForeground(new Color(191,42,117));
-//	headerText.setFont(new Font("Arial",Font.BOLD,30));
-//	f.add(headerText);
+    setSize(1200,900);   
+    setTitle("Dental Square");
+    
+    /* -------------------------------------------
+		Container setup
+	---------------------------------------------- */
+    cont = this.getContentPane();
+    cont.setLayout(null);
+//    cont.setBackground(new Color(29,8,74));
+    
+    JLabel headerText = new JLabel("All Bookings Information ");
+    headerText.setBounds(110,5,700,40);  
+	headerText.setForeground(new Color(191,42,117));
+	headerText.setFont(new Font("Arial",Font.BOLD,30));
+	cont.add(headerText);
+
+
 
     String logQuery = "SELECT * FROM `appointments`";
 	DatabaseConnect db = new DatabaseConnect();
@@ -38,13 +50,14 @@ public class Bookings {
     	}
     }
     
-    String column[]={"ID","NAME","Mobile", "ADDRESS", "DATE","TIME"};         
+    String column[]={"ID","NAME","MOBILE", "ADDRESS", "DATE","TIME"};         
     JTable jt=new JTable(data,column);    
-    jt.setBounds(100,100,1000,750);          
-    JScrollPane sp=new JScrollPane(jt);    
-    f.add(sp);         
+    jt.setBounds(110,50,1000,750);          
+    JScrollPane sp=new JScrollPane(jt); 
+    cont.add(jt);
+    cont.add(sp);         
     
-    f.setVisible(true);    
+    setVisible(true);    
 }     
 
     
